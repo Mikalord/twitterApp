2a809b75d74189111f023cc4c7137b73aa37d0ff
Mikalord <63506118+Mikalord@users.noreply.github.com>
2020-12-07 21:44:37 +0200

    added CRUD functionality with class-based views

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -13,3 +14,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('post-details', kwargs={'pk': self.pk})
