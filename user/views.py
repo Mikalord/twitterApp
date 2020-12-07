6fda005318d172da6ab04ac51d0219d114418545
Mikalord <63506118+Mikalord@users.noreply.github.com>
2020-12-07 20:03:50 +0200

    added templates and login authentication

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
 from .forms import UserRegisterForm
 
 
@@ -11,8 +12,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! Happy tweeting! {username}')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
